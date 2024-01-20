3495ea9078548d2c347d256570e497af568868ff
Prince Bharti <bhartiprince.9971@gmail.com>
2024-01-20 21:04:29 +0530

    Basic Siwft UI sample app

---
+++ LandMarkApp/LandMarkApp/LandMarkAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarkAppApp.swift
+//  LandMarkApp
+//
+//  Created by Prince Bharti on 20/01/24.
+//
+
+import SwiftUI
+
+@main
+struct LandMarkAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LandMarkApp/LandMarkApp/MapView.swift
@@ -0,0 +1,27 @@
+//
+//  MapView.swift
+//  HelloWorld
+//
+//  Created by Prince Bharti on 20/01/24.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    
+   @State private var region: MKCoordinateRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    
+    var body: some View {
+        Map(coordinateRegion: $region)
+    }
+    
+  
+    
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
